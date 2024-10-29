b053fdb2138b31185f9a0541d2cbaceae1922d7f
karol <jgorny268@gmail.com>
2024-10-29 17:19:11 +0100

    dodano folder zadanie2 z kodem w pythonie

---
+++ zadanie2/RUDY/karolgorny.py
@@ -0,0 +1,9 @@
+def get_number(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Niepoprawna wartość")
+liczba1 = get_number("Podaj liczbę1: ")
+liczba2 = get_number("Podaj liczbę2: ")
+print("Suma: ", liczba1 + liczba2)
